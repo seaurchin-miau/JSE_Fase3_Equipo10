440ee046b966bd93e1aa69a40f480c12dcb6ab69
eliasfire <vakito85@gmail.com>
2023-06-13 19:48:20 -0300

    postwork 6 wip 1

---
+++ TrabajoFinal/FinalProject/src/main/java/Postwork/FinalProject/persistence/entities/Cliente.java
@@ -0,0 +1,29 @@
+package Postwork.FinalProject.model;
+
+import Postwork.FinalProject.validation.Name;
+import jakarta.validation.constraints.*;
+
+@Data
+@Table(name = "CLIENTES")
+@Entity
+@RequiredArgsConstructor
+public class Cliente {
+
+    @PositiveOrZero(message = "El identificador no puede ser un número negativo")
+    private long id;
+
+    @NotEmpty(message = "El nombre del cliente no puede estar vacío")
+    @Size(min = 5, max = 30, message = "El nombre del cliente debe tener al menos 5 letras y ser menor a 30")
+    private String name;
+
+    @Email
+    private String email;
+
+    @Min(value = 10, message = "Los clientes con menos de 10 empleados no son válidos")
+    @Max(value = 10000, message = "Los clientes con más de 10000 empleados no son válidos")
+    private int numeroEmpleados;
+
+    @NotBlank(message = "Se debe proporcionar una dirección")
+    private String address;
+
+}
